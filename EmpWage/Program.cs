2779bf24f733a7fa9780dfdae30ce6a32accbb4d
Abhishek <abhigeu9193@gmail.com>
2022-04-27 18:36:48 +0530

    Daily Wage Problem

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -3,7 +3,7 @@
 int IS_FULL_TIME = 1;
 Random random = new Random();
 int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
+/*if (empCheck == IS_FULL_TIME)
 
     Console.WriteLine("Employee is persent");
 
@@ -11,3 +11,20 @@ else
 
     Console.WriteLine("Employee is not persent");
     Console.ReadKey();
+*/
+
+int EMP_RATE_PER_HOUR = 60;
+int empHrs = 0;
+int empWage = 0;
+
+if (empCheck == IS_FULL_TIME)
+
+    empHrs = 8;
+
+else
+
+    empHrs = 0;
+
+empWage = empHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Emp Wage :" + empWage);
+Console.ReadKey();
