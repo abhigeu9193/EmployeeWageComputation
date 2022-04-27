d4fbcf84e59f9a1cb136e47e599b20fc6fb412de
Abhishek <abhigeu9193@gmail.com>
2022-04-27 21:54:38 +0530

    Calculate Wage For a Month

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -56,43 +56,44 @@ empWage = empHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("Emp Wage :" + empWage);
 Console.ReadKey();*/
 
-/*public const*/ int IS_PART_TIME = 1;
-/*public const*/ int IS_FULL_NUMBER = 2;
-/*public const */int EMP_RATE_PER_HOUR = 60;
+/*public const* int IS_PART_TIME = 1;
+/*public const int IS_FULL_NUMBER = 2;
+/*public const nt EMP_RATE_PER_HOUR = 60;
 //Random random = new Random();
 //int empCheck = random.Next(0, 5);
 /*static void Main(string[] args)*/
 
 using System;
-class program4
+class program5
 {
     public const int IS_PART_TIME = 1;
     public const int IS_FULL_NUMBER = 2;
-    public const int EMP_RATE_PER_HOUR = 60;
+    public const int EMP_RATE_PER_TIME = 60;
+    public const int NUM_OF_WORKING_DAYS = 5;
     static void Main(string[] args)
     {
-        int empHrs = 0;
-        int empWage = 0;
-        Random random = new Random();
-        int empCheck = random.Next(0, 2);
-
-        switch (empCheck)
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
         {
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            case IS_FULL_NUMBER:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
-
-                break;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_NUMBER:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_TIME;
+            totalEmpWage += empWage;
+            Console.WriteLine("Emp wage :" + empWage);
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Emp Wage :" + empWage);
+        Console.WriteLine("Total Emp Wage :" + totalEmpWage);
         Console.ReadKey();
-
-
     }
 }
