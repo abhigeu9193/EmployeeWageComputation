50eba11f03d61d4748c27f5dcd720371d337fd38
Abhishek <abhigeu9193@gmail.com>
2022-04-27 18:53:07 +0530

    Add Part Time Wage

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,8 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Employee Wage Computation Program");
-int IS_FULL_TIME = 1;
-Random random = new Random();
-int empCheck = random.Next(0, 2);
+//int IS_FULL_TIME = 1;
+//int empCheck = random.Next(0, 2);
 /*if (empCheck == IS_FULL_TIME)
 
     Console.WriteLine("Employee is persent");
@@ -12,7 +11,7 @@ else
     Console.WriteLine("Employee is not persent");
     Console.ReadKey();
 */
-
+/*
 int EMP_RATE_PER_HOUR = 60;
 int empHrs = 0;
 int empWage = 0;
@@ -24,6 +23,34 @@ if (empCheck == IS_FULL_TIME)
 else
 
     empHrs = 0;
+
+empWage = empHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Emp Wage :" + empWage);
+Console.ReadKey();*/
+
+int IS_PART_TIME = 1;
+int IS_FULL_TIME = 5; ;
+int EMP_RATE_PER_HOUR = 20;
+//here we take variable and initialize value with 0;
+int empHrs = 0;
+int empWage = 0;
+//here we take random variable to take any random value between
+Random random = new Random();
+int empCheck = random.Next(0, 5);
+Console.WriteLine(value: empCheck);
+// here we check condition*/
+if (empCheck == IS_PART_TIME)
+
+    empHrs = 4;
+
+else if (empCheck == IS_FULL_TIME)
+
+    empHrs = 8;
+
+else
+
+    empHrs = 0;
+
 
 empWage = empHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("Emp Wage :" + empWage);
