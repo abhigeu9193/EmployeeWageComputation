3a436b2ab192f6ffa4fb1755db2ac82b0d25637e
Abhishek <abhigeu9193@gmail.com>
2022-04-27 18:14:54 +0530

    Employee Persent Absent

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Employee Wage Computation Program");
+//Console.WriteLine("Employee Wage Computation Program");
+int IS_FULL_TIME = 1;
+Random random = new Random();
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
+
+    Console.WriteLine("Employee is persent");
+
+else
+
+    Console.WriteLine("Employee is not persent");
+    Console.ReadKey();
